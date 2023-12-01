be094c8efeff59eba11b86b01d6ed173d8c78cbc
vinusham10 <vinusham05@gmail.com>
2023-12-01 14:59:08 +0530

    done

--- src/app.js
+++ src/app.js
@@ -23,11 +23,19 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    let detailedPlayers = [];
 
     // Instead of forloop use Map method
     // Code here
-
+    players.forEach((x,y) => {
+        detailedPlayers.push({
+            name:x,
+            strength: getRandomStrength(),
+            image:'images/super-' + (y + 1) + '.png',
+            type: y%2==0?"hero":"villain",
+            id:y+1
+        })
+    });
     return detailedPlayers;
 }
 
@@ -36,6 +44,32 @@ const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
+
+const view = (playerObj) => {
+    let player = document.createElement('div');
+    player.classList.add('player');
+  
+    const elements = ['image', 'name', 'strength'];
+  
+    for (const elementName of elements) {
+      let element = document.createElement(elementName === 'image' ? 'img' : 'div');
+      if (elementName === 'image') {
+        element.setAttribute('src', playerObj.image);
+        element.setAttribute('alt', '');
+      }
+      if (elementName === 'name') {
+        element.className = 'name';
+        element.textContent = playerObj.name;
+      }
+      if (elementName === 'strength') {
+        element.textContent = playerObj.strength;
+        element.className = 'strength';
+      }
+      player.appendChild(element);
+    }
+  
+    return player;
+  };
 // Build player template
 const buildPlayers = (players, type) => {
     let fragment = '';
@@ -43,8 +77,10 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
-    return fragment;
+    fragment = document.createElement("div");
+    players.filter((x) => x.type == type).forEach((x) => fragment.appendChild(view(x)));
+    return fragment.innerHTML;
+    
 }
 
 // Display players in HTML
